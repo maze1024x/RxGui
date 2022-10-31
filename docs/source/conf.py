4ff92b1e96dce23545ee7869838ad6c50b7a9b3c
maze1024x <maze1024maze@gmail.com>
2022-10-31 09:25:19 +0900

    docs: update config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'docs'
-copyright = '2022, maze1024x'
-author = 'maze1024x'
+project = 'RxGui'
+copyright = '2022, RxGui Team'
+author = 'RxGui Team'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
